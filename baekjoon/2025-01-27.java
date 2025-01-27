652e1709bab09d09d43e4401547caf72a5636e69
groom <hayaon0603@naver.com>
2025-01-28 03:06:01 +0900

    [feat] 2025-01-27 : 입력 받은 정수 오름차순 정렬 문제

---
+++ baekjoon/2025-01-27.java
@@ -0,0 +1,35 @@
+// 입력
+// 1. 수의 개수 N
+// 2. N개의 줄에 수
+
+// 출력
+// N개의 줄에 오름차순 정렬 결과
+// 한 줄에 하나씩
+
+import java.util.*;
+import java.io.*;
+
+public class  Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        // 입력받은 정수 오름차순 정렬
+        Arrays.sort(arr);
+
+        // 정렬 결과 출력
+        for(int i = 0; i < n; i++) {
+            bw.write(arr[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
